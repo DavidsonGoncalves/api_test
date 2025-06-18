23c673def164e58ffcd43a63cff5b1dda879729e
Davidson <davidson.cg7@gmail.com>
2025-06-18 19:40:28 -0300

    creating mailer files, (one of them to test)

--- app/mailers/survey_mailer.rb
+++ app/mailers/survey_mailer.rb
@@ -1,2 +1,8 @@
 class SurveyMailer < ApplicationMailer
+  default from: 'deivao.galoucura11@gmail.com'
+
+  def survey_mail(ticket)
+    @ticket = ticket
+    mail(to: @ticket.mail, subject: 'Survey Test')
+  end
 end
---
+++ app/mailers/test_mailer.rb
@@ -0,0 +1,11 @@
+class TestMailer < ApplicationMailer
+default from: 'deivao.galoucura11@gmail.com'
+
+  def test_mail
+    mail(
+      to: 'davidson.cg7@gmail.com',
+      subject: 'Test Email from Rails Application',
+      body: 'This is a test email sent from the Rails application.'
+    )
+  end
+end
