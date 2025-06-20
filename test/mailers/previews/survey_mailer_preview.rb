acdaba8c0630e0a58f1ac95265c06d34342abb08
Davidson <davidson.cg7@gmail.com>
2025-06-20 19:44:56 -0300

    configuring mail preview

--- test/mailers/previews/survey_mailer_preview.rb
+++ test/mailers/previews/survey_mailer_preview.rb
@@ -1,3 +1,6 @@
 # Preview all emails at http://localhost:3000/rails/mailers/survey_mailer
 class SurveyMailerPreview < ActionMailer::Preview
+def survey_mail
+  SurveyMailer.survey_mail(Ticket.first)
+end
 end
