aa53da6bca9f35bed4babeb2fd5bb7e8892b3332
Davidson <davidson.cg7@gmail.com>
2025-06-24 16:00:55 -0300

    Fixing authenticate_webhook method

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,7 +3,7 @@ class ApplicationController < ActionController::API
   def authenticate_webhook
    provided_token = request.headers['X-Webhook-Token']
 
-   unless provided_token.secure_compare(provided_token.to_s,  ENV['WEBHOOK_TOKEN'])
+   unless ActiveSupport::SecurityUtils.secure_compare(provided_token.to_s,  ENV['WEBHOOK_TOKEN'].to_s)
     render json: { error: 'Unauthorized' }, status: :unauthorized
     return
    end
